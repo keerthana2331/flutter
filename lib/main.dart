1da642f813d5c254fcc4a4ac79c6c52c0c4f2bce
keerthana2331 <keerthana.stackup@gmail.com>
2024-10-21 17:58:46 +0530

    added some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(MyApp());
+  runApp (MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -26,109 +26,105 @@ class Homescreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.orange,
       // appBar: AppBar(),
-      
+
       body: SafeArea(
-        child: Column(
+          child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-      Expanded(
-        flex: 2,
+          Expanded(
+            flex: 2,
             child: Container(
-                     color: Colors.yellow,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 27,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  TextButton(
-                    onPressed: () {
-                      print('Click Me');
-                    },
-                    child: Text('Click Me'),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.mic),
-                  ),
-                ]),
-                //   icon:Icons.mic),
-                // ],),
-                ElevatedButton(
-                  onPressed: () {
-                    print('Elevated Button Clicked');
-                  },
-                  child: Text('Click Me'),
-                ),
-              ],
-            ),
+              color: Colors.yellow,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
                     ),
+                  ),
+
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    TextButton(
+                      onPressed: () {
+                        print('Click Me');
+                      },
+                      child:   const Text('Click Me'),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon:  const Icon(Icons.mic),
+                    ),
+                  ]),
+                  //   icon:Icons.mic),
+                  // ],),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Elevated Button Clicked');
+                    },
+                    child:  const Text('Click Me'),
+                  ),
+                ],
+              ),
+            ),
           ),
-          
           Expanded(
             child: Container(
               decoration: BoxDecoration(
                 color: Colors.purple,
                 borderRadius: BorderRadius.circular(100),
-                border: Border.all(
-                color: Colors.blue,
-                width: 10),
+                border: Border.all(color: Colors.blue, width: 10),
               ),
-            child: Center(child: Text('B-HUB')
+              child:  const Center(child: Text('B-HUB')),
             ),
           ),
-          ),
-      Expanded(
-        
-         child:Container(
-           color: Colors.green,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 27,
-                    fontWeight: FontWeight.bold,
+          Expanded(
+            child: Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  TextButton(
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    TextButton(
+                      onPressed: () {
+                        print('Click Me');
+                      },
+                      child:  const Text('Click Me'),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon:  const Icon(Icons.mic),
+                    ),
+                  ]),
+                  //   icon:Icons.mic),
+                  // ],),
+                  ElevatedButton(
                     onPressed: () {
-                      print('Click Me');
+                      print('Elevated Button Clicked');
                     },
-                    child: Text('Click Me'),
+                    child:  const Text('Click Me'),
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.mic),
-                  ),
-                ]),
-                //   icon:Icons.mic),
-                // ],),
-                ElevatedButton(
-                  onPressed: () {
-                    print('Elevated Button Clicked');
-                  },
-                  child: Text('Click Me'),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        )
-        ],)
-      ),
+          )
+        ],
+      )),
     );
   }
 }
